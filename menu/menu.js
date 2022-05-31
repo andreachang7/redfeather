23b35d3e0ee99b166a7d83c859979592b6396ca9
andreachang7 <chana206@newschool.edu>
2022-05-31 16:30:21 -0400

    update

--- menu/menu.js
+++ menu/menu.js
@@ -1,8 +1,68 @@
-var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("appetizer-section");
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+var pro = document.getElementsByClassName("produce-section");
+var a;
+
+for (a = 0; a < pro.length; a++) {
+  pro[a].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+var ent = document.getElementsByClassName("entree-section");
+var e;
+
+for (e = 0; e < pro.length; e++) {
+  ent[e].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+var sid = document.getElementsByClassName("sides-section");
+var s;
+
+for (s = 0; s < pro.length; s++) {
+  sid[s].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+var des = document.getElementsByClassName("desserts-section");
+var d;
+
+for (d = 0; d < pro.length; d++) {
+  des[d].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
