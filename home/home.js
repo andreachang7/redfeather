7c3fe96e25d440df54eabf93ae5444fc5f69d24b
June Bee <junebee66@gmail.com>
2022-07-13 00:58:05 +0800

    sliding works!!

--- home/home.js
+++ home/home.js
@@ -1,14 +1,11 @@
 let ham = document.getElementById("v-hamburger");
-let hham = document.getElementById("h-hamburger");
 let logo = document.getElementById("v-main-logo-holder");
 let bottomfooter = document.getElementById("foot");
 let nav = document.getElementById("topnav");
 let close = document.getElementById("ham_icon");
 let gallery = document.getElementById("slider");
-let intro = document.getElementById("section-2");
+let intro = document.getElementById("home_2");
 let summary = document.getElementById("section-3");
-let imgs = document.getElementById("image-gallery");
-
 // let slider = document.getElementById("gallery_slider");
 
 // image fade
@@ -16,20 +13,14 @@ let slideIndex = 0;
 
 
 document.addEventListener("DOMContentLoaded", function(){
-  ham.style.display = "none";
-  // logo.style.display = "block";
-  intro.style.display = "block";
-  // gallery.style.display = "block";
-  imgs.style.display = "block";
-  summary.style.display = "block";
-  bottomfooter.style.display = "block";
-  nav.style.backgroundColor = "transparent";
-  close.style.backgroundImage = "url('../assets/hamburger.png')";
-  close.style.backgroundSize = "60%";
-  close.style.backgroundRepeat = "no-repeat";
-  close.style.margin = "6%";
-  close.style.marginRight = "-2%";
-  gallery.style.display = "block";
+    ham.style.display = "none";
+    console.log("laoded");
+    close.style.backgroundImage = "url('../assets/hamburger.png')";
+    close.style.backgroundSize = "60%";
+    close.style.backgroundRepeat = "no-repeat";
+    close.style.margin = "6%";
+    close.style.marginRight = "-2%";
+    // showSlides();
 });
 
 
@@ -40,8 +31,7 @@ function hamburgerToggle() {
         ham.style.display = "none";
         logo.style.display = "block";
         intro.style.display = "block";
-        // gallery.style.display = "block";
-        imgs.style.display = "block";
+        gallery.style.display = "block";
         summary.style.display = "block";
         bottomfooter.style.display = "block";
         nav.style.backgroundColor = "transparent";
@@ -50,14 +40,12 @@ function hamburgerToggle() {
         close.style.backgroundRepeat = "no-repeat";
         close.style.margin = "6%";
         close.style.marginRight = "-2%";
-        gallery.style.display = "block";
 
     } else {
         ham.style.display = "block";
         logo.style.display = "none";
         intro.style.display = "none";
-        // gallery.style.display = "none";
-        imgs.style.display = "none";
+        gallery.style.display = "none";
         summary.style.display = "none";
         bottomfooter.style.display = "none";
         nav.style.backgroundColor = "#FEFBF4";
@@ -67,7 +55,6 @@ function hamburgerToggle() {
         close.style.backgroundRepeat = "no-repeat";
         close.style.margin = "16%";
         close.style.marginRight = "10%";
-        gallery.style.display = "none";
 
     }
 }
@@ -75,21 +62,21 @@ function hamburgerToggle() {
 
 
 // showSlides();
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  console.log("testing");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   console.log("testing");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}    
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
 
